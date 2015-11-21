b0b10a7a694fedefaa02e600d6b8610c1f414fe3
antonk52 <anton52@hotmail.co.uk>
2015-11-21 19:43:46 +0300

    added description and field + html5 required attributes

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -2,21 +2,24 @@
 use yii\widgets\ListView;
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Поздравь бабулю';
 ?>
 <div class="jumbotron">
 		<p><input type="radio" value="1"> pic 1 <input type="radio" value="2"> pic 2 <input type="radio" value="1"> pic 3</p>
-        <p><input type="text" class="form-control name" placeholder="Имя" /></p>
-        <p><textarea class='form-control message' rows='3' placeholder="Пожелания"></textarea></p>
-        <p><span class="btn btn-success btn-message" href="#" role="button"><span class='glyphicon glyphicon-envelope'></span> Оставить запись</span></p>
+		<p><textarea class='form-control message' rows='2' placeholder="Пожелания" required id="congrats"></textarea></p>
+		<p><input type="text" class="form-control name" placeholder="Подпись" /></p>
+		<p><textarea class='form-control message' rows='3' placeholder="Адрес" required id="address"></textarea></p>
+		<p><input type="email" class="form-control name" placeholder="Ваш email" required id="email"></p>
+		<p><span class="btn btn-success btn-message" href="#" role="button"><span class='glyphicon glyphicon-envelope'></span> Поздравить бабулю</span></p>
+		<p class="text-muted text-justify"><small>После отправки вашей заявки вы получите имейл уведомление с номером вашей заявки и номером яндекс кошелька на который необходимо перевести оплату за услуг указав номер заказа, после этого вы получите уведомления после отправления вашей открытки.</small></p>
 </div>
 
 <div class="row marketing">
-<ul class="media-list">  
+<ul class="media-list">
   <?= ListView::widget([
-      'dataProvider' => $dataProvider,
-      'itemView' => '_item',
-      'summary' => false,
+	  'dataProvider' => $dataProvider,
+	  'itemView' => '_item',
+	  'summary' => false,
   ]);?>
 
 
