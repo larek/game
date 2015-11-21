92e514f0ad181f3072ab1d02788faedd8053833a
antonk52 <anton52@hotmail.co.uk>
2015-11-21 19:31:53 +0300

    added radio buttons

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -5,8 +5,9 @@ use yii\widgets\ListView;
 $this->title = 'My Yii Application';
 ?>
 <div class="jumbotron">
+		<p><input type="radio" value="1"> pic 1 <input type="radio" value="2"> pic 2 <input type="radio" value="1"> pic 3</p>
         <p><input type="text" class="form-control name" placeholder="Имя" /></p>
-        <p><textarea class='form-control message' rows='3' placeholder="Сообщение"></textarea></p>
+        <p><textarea class='form-control message' rows='3' placeholder="Пожелания"></textarea></p>
         <p><span class="btn btn-success btn-message" href="#" role="button"><span class='glyphicon glyphicon-envelope'></span> Оставить запись</span></p>
 </div>
 
